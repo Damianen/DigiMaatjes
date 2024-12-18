7e6cc3bde4621acc56ba952aaac30837a06aad61
Coen <kruijf.coen@gmail.com>
2024-12-18 15:05:21 +0100

    quick fix

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ import { Server } from 'socket.io';
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+const app = next({ dev, port });
 const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
